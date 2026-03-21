2559b4b1992a60e979bc03526a09e8e4c03f161e
joha1234207 <jahonoke110099@gmail.com>
2026-03-21 11:17:49 +0500

    Update admin__panel.py

--- admin__panel.py
+++ admin__panel.py
@@ -2,4 +2,4 @@
 import tugmalar
 
 def admin_panel_(bot, admin):
-		bot.send_message(admin, "<i>Salom Jahongir👋</i>\n\n☠️ADMIN PANEL VER: 2.1\n\n🤖Bot ver:2.1\n\n👀Userlarni koʻramizmi\n\n📢Elon qilamiz", reply_markup=tugmalar.admin_tugma, parse_mode="HTML")
+		bot.send_message(admin, "<i>Salom Jahongir👋</i>\n\n☠️ADMIN PANEL VER: 2.3\n\n🤖Bot ver:3.0\n\n👀Userlarni koʻramizmi\n\n📢Elon qilamiz", reply_markup=tugmalar.admin_tugma, parse_mode="HTML")
